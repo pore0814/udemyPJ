9a86afa2237dc040779a2d86ccd5eaa31f4ebdf3
002563 <anniewang@transglobe.com.tw>
2021-06-21 16:08:21 +0800

    166. Programmatic Navigation to the Edit Page         1.recipe-list click to Edit Page         2.recipe-detail click to Edit Page

--- prj-basics-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-basics-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { RecipeService } from './../recipe.service';
 import { ShoppingListService } from './../../shopping-list/shopping-list.service';
 import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { NumberSymbol } from '@angular/common';
 
 @Component({
@@ -15,7 +15,8 @@ export class RecipeDetailComponent implements OnInit {
   id:number;
 
   constructor(private recipeService:RecipeService,
-    private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     //const id = this.route.snapshot.params['id'];
@@ -27,8 +28,9 @@ export class RecipeDetailComponent implements OnInit {
     );
   }
 
-  onAddToShoppingList() {
-
+  onEditRecipe() {
+    //this.router.navigate(['../',this.id ,'edit'],{relativeTo: this.route});
+    this.router.navigate(['edit'],{relativeTo: this.route});
   }
 
 }
--- prj-basics-final/src/app/recipes/recipe-list/recipe-list.component.ts
+++ prj-basics-final/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,7 +1,7 @@
 import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
 import { Component, OnInit, Output,EventEmitter } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 
 // import { Recipe } from '../recipe.model';
@@ -16,12 +16,17 @@ export class RecipeListComponent implements OnInit {
  recipes: Recipe[];
 
   constructor(private recipeService : RecipeService,
-    private route: ActivatedRoute ) { }
+              private router: Router,
+              private route: ActivatedRoute ) { }
 
   ngOnInit() {
-     const id = this.route.snapshot.params['id'];
-     console.log('id',id);
+    //  const id = this.route.snapshot.params['id'];
+    //  console.log('id',id);
      this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipes() {
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
 }
